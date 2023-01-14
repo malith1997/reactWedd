564f8d19ebbc6722ef0a644a524f07c24f47b18f
malith2cristal <malith.gayashan@cristalcode.net>
2023-01-14 18:29:35 +0530

    change names

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import Nav from './componets/Nav';
-import FirstCard from './componets/Card1';
-// import Title from './componets/Title';
+
 import {chakraProvider}  from '@chakra-ui/react';
 import React from 'react';
-import SecondCard from './componets/Card2';
-import ThirdCard from './componets/Card3';
-import FourthCard from './componets/Card4';
+
 import Rspv from './componets/Rspv';
-import Footer from './componets/footer';
+// import Footer from './componets/footer';
 import Hero from './componets/Hero';
 import Held from './componets/held';
 import Map from './componets/map';
@@ -21,13 +18,6 @@ function App() {
       <Held />
       <Map />
       
-      {/* <Title />
-      <FirstCard />
-      <SecondCard />
-      <ThirdCard />
-      <FourthCard />
-      <Rspv />
-      <Footer /> */} 
     </React.Fragment>
     
   );
